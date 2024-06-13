97ebb5ac21d510828a87ca598d56c149d23d7089
Julieta Techenski <119064935+julietaTechenski@users.noreply.github.com>
2024-06-13 13:39:38 -0300

    strings w languages

--- app/src/main/java/com/example/itba/hci/components/cards/DeviceCard.kt
+++ app/src/main/java/com/example/itba/hci/components/cards/DeviceCard.kt
@@ -6,11 +6,11 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -19,8 +19,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -28,6 +29,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.itba.hci.R
 import com.example.itba.hci.ui.theme.HomeDomeTheme
+
 @Composable
 fun DeviceCard(
     @StringRes text: Int,
@@ -35,37 +37,46 @@ fun DeviceCard(
     @DrawableRes icon: Int? = null
 ) {
     val mediumPadding = dimensionResource(R.dimen.medium_padding)
+    val screenWidth = LocalConfiguration.current.screenWidthDp.dp
 
     Surface(
         shape = RoundedCornerShape(16.dp),
         color = Color.White,
         shadowElevation = 4.dp,
-        modifier = modifier.padding(8.dp)
+        modifier = modifier
+            .padding(8.dp)
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp)
     ) {
         Row(
             verticalAlignment = Alignment.CenterVertically,
             modifier = Modifier
-                .width(192.dp)
-                .padding(mediumPadding)
+                .padding(vertical = mediumPadding, horizontal = 16.dp)
+                .widthIn(min = 192.dp, max = screenWidth)
         ) {
             if (icon != null) {
                 Box(
                     modifier = Modifier
-                        .size(48.dp)
-                        .background(Color.Gray)
+                        .size(64.dp)
+                        .clip(RoundedCornerShape(12.dp))
+                        .background(MaterialTheme.colorScheme.background),
+                    contentAlignment = Alignment.Center
                 ) {
                     Icon(
                         painter = painterResource(id = icon),
                         contentDescription = null,
-                        modifier = Modifier.fillMaxSize()
+                        modifier = Modifier
+                            .size(48.dp)
+                            .padding(8.dp)
                     )
                 }
+                Spacer(modifier = Modifier.width(mediumPadding)) // Ajusta el espacio aquí
             }
-            Spacer(modifier = Modifier.width(mediumPadding))
             Text(
                 text = stringResource(text),
-                style = MaterialTheme.typography.titleSmall,
-                color = Color.Black
+                style = MaterialTheme.typography.bodyLarge,
+                color = Color.Black,
+                modifier = Modifier.padding(start = mediumPadding) // Ajusta el padding aquí
             )
         }
     }
@@ -74,12 +85,12 @@ fun DeviceCard(
 
 @Preview(showBackground = true)
 @Composable
-fun FavouriteCardPreview() {
+fun DeviceCardPreview() {
     val smallPadding = dimensionResource(R.dimen.small_padding)
 
     HomeDomeTheme {
         DeviceCard(
-            text = R.string.app_name,
+            text = R.string.bottom_navigation_devices,
             modifier = Modifier.padding(smallPadding),
             icon = R.drawable.devices
         )
--- app/src/main/java/com/example/itba/hci/ui/theme/Type.kt
+++ app/src/main/java/com/example/itba/hci/ui/theme/Type.kt
@@ -14,7 +14,48 @@ val Typography = Typography(
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
+    ),
+    titleLarge = TextStyle(
+        fontSize = 30.sp,
+        fontWeight = FontWeight.Bold,
+        letterSpacing = 0.sp
+    ),
+    titleMedium = TextStyle(
+        fontSize = 24.sp,
+        fontWeight = FontWeight.Bold,
+        letterSpacing = 0.sp
+    ),
+    titleSmall = TextStyle(
+        fontSize = 20.sp,
+        fontWeight = FontWeight.Bold,
+        letterSpacing = 0.sp
+    ),
+    bodyMedium = TextStyle(
+        fontSize = 16.sp,
+        fontWeight = FontWeight.Normal,
+        letterSpacing = 0.sp
+    ),
+    bodySmall = TextStyle(
+        fontSize = 14.sp,
+        fontWeight = FontWeight.Normal,
+        letterSpacing = 0.sp
+    ),
+    labelLarge = TextStyle(
+        fontSize = 16.sp,
+        fontWeight = FontWeight.Medium,
+        letterSpacing = 0.15.sp
+    ),
+    labelMedium = TextStyle(
+        fontSize = 14.sp,
+        fontWeight = FontWeight.Medium,
+        letterSpacing = 0.1.sp
+    ),
+    labelSmall = TextStyle(
+        fontSize = 12.sp,
+        fontWeight = FontWeight.Medium,
+        letterSpacing = 0.1.sp
     )
+
     /* Other default text styles to override
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
