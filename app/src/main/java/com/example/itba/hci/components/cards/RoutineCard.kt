08763ed6fe1b3e97733e16c62eb1ab24cfc0c824
valentinamartireta <vmartireta@itba.edu.ar>
2024-06-15 20:37:52 -0300

    commit supressing of warnings

--- app/src/main/java/com/example/itba/hci/components/cards/RoutineCard.kt
+++ app/src/main/java/com/example/itba/hci/components/cards/RoutineCard.kt
@@ -1,15 +1,11 @@
 package com.example.itba.hci.components.cards
 
-import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -31,7 +27,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
@@ -39,7 +34,6 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.itba.hci.R
-import com.example.itba.hci.components.CircleIcon
 import com.example.itba.hci.ui.theme.HomeDomeTheme
 
 @Composable
