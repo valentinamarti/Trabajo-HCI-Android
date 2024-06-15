5b49313fb9870b7030194a02fc78d09cbf56831c
valentinamartireta <vmartireta@itba.edu.ar>
2024-06-15 19:02:51 -0300

    commit implementation of top bar

--- app/src/main/java/com/example/itba/hci/components/TopAppBar.kt
+++ app/src/main/java/com/example/itba/hci/components/TopAppBar.kt
@@ -9,14 +9,11 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.itba.hci.R
