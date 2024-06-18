e8eabffa03d597e4271c58d65851f87ef86ad808
nicanorPorto <119369734+nicanorPorto@users.noreply.github.com>
2024-06-18 10:17:50 -0300

    Updated nav so that it displays the device component when clicking on a device card

--- app/src/main/java/com/example/itba/hci/utils/NavigationBarGraph.kt
+++ app/src/main/java/com/example/itba/hci/utils/NavigationBarGraph.kt
@@ -1,14 +1,20 @@
 package com.example.itba.hci.utils
 
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
 import com.example.itba.hci.ui.screens.DevicesScreen
 import com.example.itba.hci.ui.screens.HomeScreen
 import com.example.itba.hci.ui.screens.RoutinesScreen
-
+import com.example.itba.hci.ui.components.devices.BlindsCard
+import com.example.itba.hci.ui.components.devices.DoorCard
+import com.example.itba.hci.ui.components.devices.FridgeCard
+import com.example.itba.hci.ui.components.devices.SpeakerCard
 
 @Composable
 fun NavigationBarGraph(navController: NavHostController, paddingValues: PaddingValues) {
@@ -16,5 +22,22 @@ fun NavigationBarGraph(navController: NavHostController, paddingValues: PaddingV
         composable(NavigationScreen.Routines.route) { RoutinesScreen(navController, paddingValues) }
         composable(NavigationScreen.Home.route) { HomeScreen(navController, paddingValues) }
         composable(NavigationScreen.Devices.route) { DevicesScreen(navController, paddingValues) }
+        composable(
+            "deviceDetail/{deviceType}/{deviceId}",
+            arguments = listOf(
+                navArgument("deviceType") { type = NavType.StringType },
+                navArgument("deviceId") { type = NavType.StringType }
+            )
+        ) { backStackEntry ->
+            val deviceType = backStackEntry.arguments?.getString("deviceType") ?: ""
+            val deviceId = backStackEntry.arguments?.getString("deviceId") ?: ""
+            when (deviceType) {
+                "Parlante" -> SpeakerCard(deviceId)
+                "Heladera" -> FridgeCard(deviceId)
+                "Persiana" -> BlindsCard(deviceId)
+                "Puerta" -> DoorCard(deviceId)
+                else -> Text("Unknown Device")
+            }
+        }
     }
 }
