2c66e8ae5a9ee55adc320096db8ecb8124e5f6df
Julieta Techenski <119064935+julietaTechenski@users.noreply.github.com>
2024-06-21 17:33:40 -0300

    34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.itba.hci"
-        minSdk = 29
+        minSdk = 34
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
