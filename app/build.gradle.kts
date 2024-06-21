2f0638cdc27a2f2073bb443d1305a16fdb9f0bb9
valentinamartireta <vmartireta@itba.edu.ar>
2024-06-21 18:06:43 -0300

    commit fixing of bug

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.itba.hci"
-        minSdk = 34
+        minSdk = 29
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
